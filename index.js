c823279c09cbdb2ba68683214d6c5673206b4b01
fjmpy6hm2b-cloud <317653701+fjmpy6hm2b-cloud@users.noreply.github.com>
2026-08-31 22:47:19 -0500

    Install pending command reconnect recovery

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import { SleepysBedController } from './bedController.js';
+import { installCommandRecovery } from './commandRecovery.js';
 import { SleepysElitePlatform } from './platform.js';
 import { PLATFORM_NAME } from './settings.js';
+
+installCommandRecovery(SleepysBedController);
 
 export default (api) => {
   api.registerPlatform(PLATFORM_NAME, SleepysElitePlatform);
